d1a965cd22f7368bb77c88c977f41a2ee692c569
ROHITKR2 <starrohitgks@gmail.com>
2024-07-25 08:04:37 +0000

    Pending changes exported from your codespace

---
+++ q2.py
@@ -0,0 +1,8 @@
+
+name= str(input("enter your name:"))
+age= int(input("enter your age: "))
+fav_hobby=str(input("enter your fav hobby:"))
+
+print("Name:" , name)
+print("Age:" , age)
+print("Fav hobby:" , fav_hobby)
